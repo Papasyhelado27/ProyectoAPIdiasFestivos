94c384eb51dfd9490d21b3fd6269f7c85a20ca18
Barrera433 <brayanstbv@gmail.com>
2025-05-08 22:18:05 -0500

    Update OperacionesFechasServicio.java
    Logica para fechas tipo 3 y tipo 4 
    
    Signed-off-by: Barrera433 <brayanstbv@gmail.com>

--- diasfestivos/api/aplicacion/src/main/java/servicios/OperacionesFechasServicio.java
+++ diasfestivos/api/aplicacion/src/main/java/servicios/OperacionesFechasServicio.java
@@ -25,7 +25,7 @@ public class OperacionesFechasServicio implements IOperacionesFechasServicio {
         calendario.add(Calendar.DATE, dias);
         return calendario.getTime();
     }
-    
+
     public Date obtenerSiguienteLunes(Date fecha){
         Calendar calendario = Calendar.getInstance();
         calendario.setTime(fecha);
@@ -33,13 +33,12 @@ public class OperacionesFechasServicio implements IOperacionesFechasServicio {
         int diaSemana = calendario.get(Calendar.DAY_OF_WEEK);
         if(diaSemana != Calendar.MONDAY) {
             if (diaSemana > Calendar.MONDAY)
-                fecha = agregarDias(fecha, 9 - diaSemana);
+                calendario.add(Calendar.DATE, 9 - diaSemana);
             else
-                fecha = agregarDias(fecha, 1);
+                calendario.add(Calendar.DATE, Calendar.MONDAY - diaSemana);
         }
 
-        return fecha;
-
+        return calendario.getTime();
     }
 
     // obtener inicio de Semana Sante
@@ -51,65 +50,95 @@ public class OperacionesFechasServicio implements IOperacionesFechasServicio {
 
         int dias = d + (2 * b + 4 * c + 6 * d + 5) % 7;
 
-        int dia = 15 + dias;
-        int mes = 3;
+        Calendar calendario = Calendar.getInstance();
+        calendario.set(año, Calendar.MARCH, 15);
+        calendario.add(Calendar.DATE, dias);
 
-        return new Date(año - 1900, mes - 1, dia);
-
+        return calendario.getTime();
     }
 
-    public Date obtenerFechaDomingoPascua(int año){ 
-
-        return obtenerDomingoRamos(año);
-
+    public Date obtenerFechaDomingoPascua(int año){
+        Calendar calendario = Calendar.getInstance();
+        calendario.setTime(obtenerDomingoRamos(año));
+        calendario.add(Calendar.DATE, 7);
+        return calendario.getTime();
     }
 
     public boolean validarPorFestivoTipo1(int año, int mes, int dia){
-
         return !festivoServicio.buscarPorDiaMesTipo1(dia, mes).isEmpty();
-
     }
 
     public boolean validarPorFestivoTipo2(int año, int mes, int dia){
-
         List<Festivo> festivosTipo2 =  festivoServicio.buscarPorTipo(2);
-
-        Date siguienteLunes;
+        Calendar calendarioFechaConsulta = Calendar.getInstance();
+        calendarioFechaConsulta.set(año, mes - 1, dia);
+        Date fechaConsulta = calendarioFechaConsulta.getTime();
 
         for(Festivo festivo : festivosTipo2) {
-            Date fechaFestivo = new Date(año - 1900, festivo.getMes(), festivo.getDia());
-            siguienteLunes = obtenerSiguienteLunes(fechaFestivo);
-            
-            if(fechaFestivo.equals(siguienteLunes))
+            Calendar calendarioFechaFestivo = Calendar.getInstance();
+            calendarioFechaFestivo.set(año, festivo.getMes() - 1, festivo.getDia());
+            Date fechaFestivoOriginal = calendarioFechaFestivo.getTime();
+            Date siguienteLunes = obtenerSiguienteLunes(fechaFestivoOriginal);
+
+            if(fechaConsulta.equals(siguienteLunes)) {
                 return true;
+            }
         }
-
         return false;
-
     }
 
     public boolean validarPorFestivoTipo3(int año, int mes, int dia){
-
         List<Festivo> festivosTipo3 =  festivoServicio.buscarPorTipo(3);
+        Calendar calendarioFechaConsulta = Calendar.getInstance();
+        calendarioFechaConsulta.set(año, mes - 1, dia);
+        Date fechaConsulta = calendarioFechaConsulta.getTime();
 
+        Date domingoPascua = obtenerFechaDomingoPascua(año);
+        Calendar calendarioPascua = Calendar.getInstance();
+        calendarioPascua.setTime(domingoPascua);
+
+        for (Festivo festivo : festivosTipo3) {
+            Calendar calendarioFestivo = Calendar.getInstance();
+            calendarioFestivo.setTime(domingoPascua);
+            calendarioFestivo.add(Calendar.DATE, festivo.getEasterOffset());
+            if (calendarioFechaConsulta.get(Calendar.YEAR) == calendarioFestivo.get(Calendar.YEAR) &&
+                    calendarioFechaConsulta.get(Calendar.MONTH) == calendarioFestivo.get(Calendar.MONTH) &&
+                    calendarioFechaConsulta.get(Calendar.DAY_OF_MONTH) == calendarioFestivo.get(Calendar.DAY_OF_MONTH)) {
+                return true;
+            }
+        }
         return false;
-
     }
 
     public boolean validarPorFestivoTipo4(int año, int mes, int dia){
-
         List<Festivo> festivosTipo4 =  festivoServicio.buscarPorTipo(4);
+        Calendar calendarioFechaConsulta = Calendar.getInstance();
+        calendarioFechaConsulta.set(año, mes - 1, dia);
+        Date fechaConsulta = calendarioFechaConsulta.getTime();
 
+        Date domingoPascua = obtenerFechaDomingoPascua(año);
+
+        for (Festivo festivo : festivosTipo4) {
+            Calendar calendarioFestivo = Calendar.getInstance();
+            calendarioFestivo.setTime(domingoPascua);
+            calendarioFestivo.add(Calendar.DATE, festivo.getEasterOffset());
+            Date fechaFestivoOriginal = calendarioFestivo.getTime();
+            Date siguienteLunes = obtenerSiguienteLunes(fechaFestivoOriginal);
+
+            if (fechaConsulta.equals(siguienteLunes)) {
+                return true;
+            }
+        }
         return false;
-
     }
 
     public boolean validarFechaFestivo(int año, int mes, int dia){
-        
-        // Si devuelve algo al buscar el Festivo, entonces es festivo
+
+        // Si devuelve algo al buscar Festivo, entonces es festivo
         if(validarPorFestivoTipo1(año, mes, dia)) {
             return true;
-        } else if (validarPorFestivoTipo2(año, mes, dia)) { // Si no devuelve nada, validar que es tipo 2
+            // Si no devuelve nada, validar que es tipo 2
+        } else if (validarPorFestivoTipo2(año, mes, dia)) { 
             return true;
         } else if (validarPorFestivoTipo3(año, mes, dia)) {
             return true;
@@ -118,8 +147,7 @@ public class OperacionesFechasServicio implements IOperacionesFechasServicio {
         }
 
         return false;
-
     }
 
-    
+
 }
